a33c59408ee8a20f4a6ef789c52bc464a7363ce8
netia-melvin <melvinatemba18@gmail.com>
2024-11-21 09:05:12 +0000

    fixes

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,117 +1,67 @@
 #!/usr/bin/env python3
-"""A simple Flask app with user authentication features.
 """
-from flask import Flask, jsonify, request, abort, redirect
+Module for creating user database
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from auth import Auth
+Base = declarative_base()
 
 
-app = Flask(__name__)
-AUTH = Auth()
-
-
-@app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
-    """GET /
-    Return:
-        - The home page's payload.
+class User(Base):
     """
-    return jsonify({"message": "Bienvenue"})
-
-
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
-    """POST /users
-    Return:
-        - The account creation payload.
+    Creates/TRepresents table for users
     """
-    email, password = request.form.get("email"), request.form.get("password")
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() -> str:
-    """POST /sessions
-    Return:
-        - The account login payload.
+Task 2
+Right way to import is highlighted in red
+
+#!/usr/bin/env python3
+"""
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+class DB:
     """
-    email, password = request.form.get("email"), request.form.get("password")
-    if not AUTH.valid_login(email, password):
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
-
-
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout() -> str:
-    """DELETE /sessions
-    Return:
-        - Redirects to home route.
+    DB class
     """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
 
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        # either set echo=False or remove it entirely as the default value is false
+        self._engine = create_engine("sqlite:///a.db")
+        # Drops existing tables
+        Base.metadata.drop_all(self._engine)
+        # creates new ones based on the models defined in user.py.
+        Base.metadata.create_all(self._engine)
+        # Initializes the private __session attribute to None.
+        self.__session = None
 
-@app.route("/profile", methods=["GET"], strict_slashes=False)
-def profile() -> str:
-    """GET /profile
-    Return:
-        - The user's profile information.
-    """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    return jsonify({"email": user.email})
+    @property
+    def _session(self) -> Session:
+        """
+        Memoize session object
+        """
+        # Checks if the private __session attribute is None.
+        if self.__session is None:
+            # If it is, creates a new DBSession using the database
+            #   engine and assigns it to __session.
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+            # Returns the current value of __session.
+        return self.__session
 
-
-@app.route("/reset_password", methods=["POST"], strict_slashes=False)
-def get_reset_password_token() -> str:
-    """POST /reset_password
-    Return:
-        - The user's password reset payload.
-    """
-    email = request.form.get("email")
-    reset_token = None
-    try:
-        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
-        reset_token = None
-    if reset_token is None:
-        abort(403)
-    return jsonify({"email": email, "reset_token": reset_token})
-
-
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
-def update_password() -> str:
-    """PUT /reset_password
-
-    Return:
-        - The user's password updated payload.
-    """
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    is_password_changed = False
-    try:
-        AUTH.update_password(reset_token, new_password)
-        is_password_changed = True
-    except ValueError:
-        is_password_changed = False
-    if not is_password_changed:
-        abort(403)
-    return jsonify({"email": email, "message": "Password updated"})
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
